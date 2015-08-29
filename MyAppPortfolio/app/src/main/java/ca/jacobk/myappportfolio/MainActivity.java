8e8ebe03472efcfdef673d985d76293e5b1c62e9
Jacob Kwitkoski <jacobkwitkoski@gmail.com>
2015-08-29 08:13:16 -0700

    Applying most of the feedback from the portfolio submission: - Updated IDs for views and buttons to be more descriptive - Used style for common button attributes. - Commented out Settings button usage, as settings does not do anything (yet)
    Still TODO: change from RelativeLayout to LinearLayout

--- MyAppPortfolio/app/src/main/java/ca/jacobk/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/ca/jacobk/myappportfolio/MainActivity.java
@@ -16,27 +16,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    /// Disabling for now, no need to have an options menu at this point
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     public void buttonPopularMoviesOnClick(View v) {
         Resources res = getResources();
